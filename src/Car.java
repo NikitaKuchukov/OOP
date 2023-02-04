041141519bbf2e594e5f59b51b7f8b2cd5405677
NikitaKuchukov <Kuchukovnik@mail.ru>
2023-02-04 21:50:59 +0700

    Homework done

--- src/Car.java
+++ src/Car.java
@@ -7,12 +7,12 @@ public class Car {
     String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = (brand == null || brand.equals("") ? "default" : brand);
-        this.model = (model == null || model.equals("") ? "default" : model);
+        this.brand = (brand == null || brand.isEmpty() ? "default" : brand);
+        this.model = (model == null || model.isEmpty() ? "default" : model);
         this.engineVolume = (engineVolume <= 0 ? 1.5 : engineVolume);
-        this.color = (color == null || color.equals("") ? "White" : color);
+        this.color = (color == null || color.isEmpty() ? "White" : color);
         this.year = (year <= 0 ? 2000 : year);
-        this.country = (country == null || country.equals("") ? "default" : country);
+        this.country = (country == null || country.isEmpty() ? "default" : country);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Car Lada = new Car("Lada", "Granta", 0, "Yellow", 2015, "Russia");
-        Car Audi = new Car("", "A8 50 L", 3.0, "Black", 2020, "Germany");
-        Car BMW = new Car("BMW", null, 3.0, "Black", -3, "Germany");
-        Car Kia = new Car("Kia", "", 2.4, "Red", 2018, "South Korea");
-        Car Hyundai = new Car("Hyundai", "Avante", 1.6, null, 2016, "South Korea");
+        Car[] cars = new Car[5];
+        cars[0] = new Car("Lada", "Granta", 0, "Yellow", 2015, "Russia");
+        cars[1] = new Car("", "A8 50 L", 3.0, "Black", 2020, "Germany");
+        cars[2] = new Car("BMW", null, 3.0, "Black", -3, "Germany");
+        cars[3] = new Car("Kia", "", 2.4, "Red", 2018, "South Korea");
+        cars[4] = new Car("Hyundai", "Avante", 1.6, null, 2016, "South Korea");
 
-        System.out.println(Lada);
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+     /*   System.out.println(Lada);
         System.out.println(Audi);
         System.out.println(BMW);
         System.out.println(Kia);
         System.out.println(Hyundai);
-    }
+    */}
 }
